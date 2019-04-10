ca3e8241095fae85d4993e969677f4a10beac400
Molly Nelson <mollykathrynnelson@gmail.com>
2019-04-10 02:45:13 +0000

    Automatically backed up by Learn

---
+++ matcha-fika/lib/matcha/fika.rb
@@ -0,0 +1,7 @@
+require "matcha/fika/version"
+
+module Matcha
+  module Fika
+    # Your code goes here...
+  end
+end
---
+++ matcha-fika/lib/matcha/fika/version.rb
@@ -0,0 +1,5 @@
+module Matcha
+  module Fika
+    VERSION = "0.1.0"
+  end
+end
